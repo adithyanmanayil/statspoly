0a6d56ad41e44a188e5418ec8ad534a1226d3bec
adithyanmanayil <115716906+adithyanmanayil@users.noreply.github.com>
2023-04-16 19:48:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -33,32 +33,56 @@ if ($conn->connect_error){
                             <input type="submit" class='btn-ppt' value='Login' name='login'>
                         </form>
                         <?php
-                        session_start();
-                        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                            if(isset($_POST['login'])){
-                                $user=$_POST['user'];
-                                $pwd=$_POST['pwd'];
-                                $sql="SELECT * FROM registration WHERE password='$pwd' AND mobile='$user'";
-                                $result = $conn->query($sql);
-                                while ($row = mysqli_fetch_array($result)) {
-                                    if ($row['mobile']==$user){
-                                        $_SESSION['user_admn'] = $row['admn'] ;
-                                        $_SESSION['user_sem'] = $row['sem'] ;
-                                        $_SESSION['user_type'] = $row['type'] ;
-                                        if($row['type']==0){
-                                            header("Location:student.php");
-                                        }
-                                        if($row['type']==1){
-                                            header("Location:tutor.php");
-                                        }
-                                        if($row['type']==2){
-                                            header("Location:admin.php");
-                                        }
-                                        exit;
-                                    }
-                                }
-                            }
-                        }
+                       session_start();
+
+                       if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                           if(isset($_POST['login'])){
+                               $user=$_POST['user'];
+                               $pwd=$_POST['pwd'];
+                               $sql="SELECT * FROM registration WHERE mobile='$user'";
+                               $result = $conn->query($sql);
+                               if ($result->num_rows > 0) {
+                                   $row = $result->fetch_assoc();
+                                   if ($row['password'] === $pwd){
+                                       // Password matches
+                                       $_SESSION['user_admn'] = $row['admn'] ;
+                                       $_SESSION['user_pwd'] = $row['password'];
+                                       $_SESSION['user_sem'] = $row['sem'] ;
+                                       $_SESSION['user_type'] = $row['type'] ;
+                                       if($row['type']==0){
+                                           header("Location:student.php");
+                                           exit;
+                                       }
+                                       if($row['type']==1){
+                                           header("Location:tutor.php");
+                                           exit;
+                                       }
+                                       if($row['type']==2){
+                                           header("Location:admin.php");
+                                           exit;
+                                       }
+                                   } else {
+                                       echo "Incorrect password";
+                                   }
+                               } else {
+                                   echo "User not found";
+                               }
+                           }
+                       }
+                       
+                       // Display error message if there was an error
+                       if (isset($error)) {
+                           echo "<p>Error: $error</p>";
+                       }
+                       
+                       // Check if user is logged in and redirect to login page if not
+                       if (!isset($_SESSION['user_admn'])) {
+                           header('Location: index.php');
+                           exit;
+                       }
+                       
+                       // User is logged in, display page content here
+                       
                         ?>
                     </center>
                 </div>
