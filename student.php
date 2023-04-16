09f4550dc217b2846b27087a48be4137b3355be6
adithyanmanayil <115716906+adithyanmanayil@users.noreply.github.com>
2023-04-16 19:49:13 +0530

    Update student.php

--- student.php
+++ student.php
@@ -83,7 +83,7 @@ $usertype=$_SESSION['user_type'];
 				echo"<div class='card-03' style='padding: 2em;'>";
 				echo"<h3>SEMESTER 0$i</h3>";
 				echo"<form method=POST style='margin: 2em auto;'>";
-				echo"<table style='width: 100%; margin: 0 auto; border=0;'>";
+				echo"<table class='tables' style='width: 100%; margin: 0 auto; border=0;'>";
 				echo"<tr><th style='padding: 0 0.4em;'>CODE</th><th style='padding: 0 0.4em;'>NAME</th><th style='padding: 0 0.4em;'>CREDIT</th><th style='padding: 0 0.4em;'>Imark</th><th colspan='7'>GRADE</th></tr>";
 				echo"<tr><td></td><td></td><td></td><td></td><td style='letter-spacing: 1em; padding-left: 0.8em;'>SABCDEF</td>";
 				$sql="SELECT * FROM subjects where sem=$i AND 'type'=$usertype";
@@ -116,57 +116,6 @@ $usertype=$_SESSION['user_type'];
 						$j++;
 					}
 				}
-				//$sql2="SELECT subjects.code, subjects.name, subjects.credit, subjects.sem, results.grade, registration.admn
-				//	FROM subjects
-				//	INNER JOIN results ON results.code=subjects.code					
-				//	INNER JOIN registration ON registration.admn = results.admn
-				//	WHERE subjects.sem=$i AND registration.type=$usertype";
-				//$result2=$conn->query($sql2);
-				//if($result2->num_rows>0){
-				//	$j=1;
-				//	while($row2=$result2->fetch_assoc()){
-				//		$ssem = "co" .$i.$j;
-				//		$grade = "g" .$i.$j;
-				//		echo "<tr>";
-				//		echo "<td style='padding: 1em; width: 10%;'>
-				//		<input type='text' name='$ssem' value=".$row2["code"]." readonly style='font-weight: 700; text-align: center; width: 100%;'>
-				//		</td>";
-				//		echo "<td style='text-align: left; width: 50%;'>".$row2["name"]."</td>";
-				//		echo "<td style='width: 5%;'>".$row2["credit"]."</td>";
-				//		echo "<td style='width: 35%;'>";
-				//		echo "<input type='radio' name='$grade' value='S' style='margin: 0 0.5em;'";
-				//		if($row2['grade']=="S"){
-				//			echo "checked>";
-				//		}else{echo">";}
-				//		echo "<input type='radio' name='$grade' value='A' style='margin: 0 0.5em;'";
-				//		if($row2['grade']=="A"){
-				//			echo "checked>";
-				//		}else{echo">";}
-				//		echo "<input type='radio' name='$grade' value='B' style='margin: 0 0.5em;'";
-				//		if($row2['grade']=="B"){
-				//			echo "checked>";
-				//		}else{echo">";}
-				//		echo "<input type='radio' name='$grade' value='C' style='margin: 0 0.5em;'";
-				//		if($row2['grade']=="C"){
-				//			echo "checked>";
-				//		}else{echo">";}
-				//		echo "<input type='radio' name='$grade' value='D' style='margin: 0 0.5em;'";
-				//		if($row2['grade']=="D"){
-				//			echo "checked>";
-				//		}else{echo">";}
-				//		echo "<input type='radio' name='$grade' value='E' style='margin: 0 0.5em;'";
-				//		if($row2['grade']=="E"){
-				//			echo "checked>";
-				//		}else{echo">";}
-				//		echo "<input type='radio' name='$grade' value='F' style='margin: 0 0.5em;'";
-				//		if($row2['grade']=="F"){
-				//			echo "checked>";
-				//		}else{echo">";}
-				//		echo "</td>";
-				//		echo "</tr>";
-				//		$j++;
-				//	}
-				//}
 				echo "</table>";
 				echo "<input type='submit' class='btn-ppt' name='sem$i' value='Save'>";
 				echo "</form>";
@@ -186,11 +135,11 @@ $usertype=$_SESSION['user_type'];
 								$conn->query($sql21);
 								$sql22="UPDATE results SET imark='$simark' WHERE admn='$user' AND code='$scode'";
 								$conn->query($sql22);
-								$sql23="UPDATE results SET sem='$usersem' WHERE admn='$user' AND code='$scode'";
+								$sql23="UPDATE results SET sem='$i' WHERE admn='$user' AND code='$scode'";
 								$conn->query($sql23);
 							}
 							else{
-								$sql22="INSERT INTO results (admn, code, grade, imark, sem) VALUES ('$user', '$scode', '$sgrade', '$simark', '$usersem')";
+								$sql22="INSERT INTO results (admn, code, grade, imark, sem) VALUES ('$user', '$scode', '$sgrade', '$simark', '$i')";
 								$conn->query($sql22);
 							}
 							$y++;
